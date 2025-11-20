c4695996444745632aeafd9eb0948269126cdb60
XMASTA007 <92289753+XMASTA007@users.noreply.github.com>
2025-11-20 13:20:15 +0200

    Script update

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,26 +1,16 @@
 const toggleSwitch = document.querySelector('input[type=checkbox]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const textBox = document.getElementById('text-box');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
 
-//Dark or Light images
-function imageMode(mode) {
-    image1.src = `img/undraw_remote_${mode}.svg`;
-    image2.src = `img/undraw_nature_${mode}.svg`;
-    image3.src = `img/undraw_starry_${mode}.svg`;
-}
 
 //Dark Mode Styles
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     //console.log(toggleIcon.children);
-    toggleIcon.children[0].textContent = 'Goth Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');   
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('toggle-sun', 'toggle-moon');
+ 
 }
 
 //Light Mode
@@ -28,8 +18,7 @@ function lightMode() {
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+    toggleIcon.children[1].classList.replace('toggle-moon', 'toggle-sun');
 }
 
 //Switch Theme
